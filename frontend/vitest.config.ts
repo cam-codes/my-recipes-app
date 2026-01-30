0879a6b58a32e07dadfe963427f69054991bf42c
Cameron Fournier <cam.codes.15@gmail.com>
2026-01-30 08:58:29 -0600

    fix frontend tests

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
+  plugins: [solid()],
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
     deps: {
       inline: ['solid-js', '@solidjs/testing-library'],
     },

b10a0fe07449f687f05dc3bbb534d0da61ec2896
Cameron Fournier <cam.codes.15@gmail.com>
2026-02-02 01:32:58 -0600

    prettier fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
+      },
+    },
   },
   build: {
     commonjsOptions: {

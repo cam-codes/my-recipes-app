b0acb32b1a71fd8282848b9d850dccbe938f51d8
Cameron Fournier <cam.codes.15@gmail.com>
2026-02-10 20:35:45 -0600

    remove unused import

--- frontend/e2e/__tests__/health.spec.ts
+++ frontend/e2e/__tests__/health.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-import {request} from "../../../backend/tests/fixtures/utils/utils.ts";
 
 test('backend health check is reachable', async ({ request }) => {
   const res = await request.get('/api/health');

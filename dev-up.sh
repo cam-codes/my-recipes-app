d387c02971007b1a1e26e4bd32038ca3f0123cb3
Cameron Fournier <cam.codes.15@gmail.com>
2026-02-10 20:27:58 -0600

    refactor build-info

--- dev-up.sh
+++ dev-up.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
+top_level=$(git rev-parse --show-toplevel)
 # Compute real values
-COMMIT=$(git rev-parse HEAD)
-TAG=$(git describe --tags --exact-match 2>/dev/null || echo "")
+GIT_COMMIT=$(git rev-parse HEAD)
+GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || echo "")
 LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "")
 
-echo "🚀 Starting local dev with commit $COMMIT and tag '$TAG'"
+echo "🚀 Starting local dev with commit '$GIT_COMMIT' and tag '$GIT_TAG' and latest tag: '$LATEST_TAG'"
 
-docker-compose -f docker-compose.yml -f docker-compose.local.yml build \
-  --build-arg GIT_COMMIT=$COMMIT \
-  --build-arg LATEST_TAG=$LATEST_TAG \
-  --build-arg GIT_TAG=$TAG
+docker-compose -f $top_level/docker-compose.yml -f $top_level/docker-compose.local.yml build \
+  --build-arg GIT_COMMIT=$GIT_COMMIT \
+  --build-arg GIT_TAG=$GIT_TAG \
+  --build-arg LATEST_TAG=$LATEST_TAG
 
-docker-compose -f docker-compose.yml -f docker-compose.local.yml up
+docker-compose -f $top_level/docker-compose.yml -f $top_level/docker-compose.local.yml up -d

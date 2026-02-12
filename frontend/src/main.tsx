55755f80ce41175a2f5882e838c48e734b8f3587
Cameron Fournier <cam.codes.15@gmail.com>
2026-02-11 22:48:28 -0600

    move GA init to index.tsx (true app entry point)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,7 @@
 import { render } from 'solid-js/web';
 import App from './App';
 import './index.css';
-import { initGA } from './lib/analytics';
 
-console.log('init ga from main.tsx')
-initGA();
-
-console.log('render...')
 render(
   () => (
       <App />
